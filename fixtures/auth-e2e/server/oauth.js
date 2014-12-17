c782cb1d9b97f49ce0c76fcb765e1b51cffd1d73
Evan You <yyx990803@gmail.com>
2014-12-17 17:16:07 -0500

    determine oauth loginStyle based on Meteor.settings

--- fixtures/auth-e2e/server/oauth.js
+++ fixtures/auth-e2e/server/oauth.js
@@ -23,6 +23,8 @@ Meteor.startup(function () {
     return secrets[provider];
   };
 
+  var loginStyle = secrets.loginStyle || 'popup';
+
   // intentionally calling the configureLoginService method
   // here instead of direct insert, since this is the method
   // being called on the server when an actual user configures it.
@@ -30,43 +32,43 @@ Meteor.startup(function () {
     service: "facebook",
     appId: "461508060653790",
     secret: secret("facebook"),
-    loginStyle: "popup"
+    loginStyle: loginStyle
   });
   Meteor.call('configureLoginService', {
     service: "google",
     clientId: "262689297883-d14f2erj5dlfhk6nlvhcldhq0624op7q.apps.googleusercontent.com",
     secret: secret("google"),
-    loginStyle: "popup"
+    loginStyle: loginStyle
   });
   Meteor.call('configureLoginService', {
     service: "twitter",
     consumerKey: "0eRD2Z28NjAMjQGeqFXM8MMY5",
     secret: secret("twitter"),
-    loginStyle: "popup"
+    loginStyle: loginStyle
   });
   Meteor.call('configureLoginService', {
     service: "github",
     clientId: "d4b54e5ba3611bc14c06",
     secret: secret("github"),
-    loginStyle: "popup"
+    loginStyle: loginStyle
   });
   Meteor.call('configureLoginService', {
     service: "weibo",
     clientId: "819563028",
     secret: secret("weibo"),
-    loginStyle: "popup"
+    loginStyle: loginStyle
   });
   Meteor.call('configureLoginService', {
     service: "meetup",
     clientId: "mvnukfi6bdoacs03tkirj4394n",
     secret: secret("meetup"),
-    loginStyle: "popup"
+    loginStyle: loginStyle
   });
   Meteor.call('configureLoginService', {
     service: "meteor-developer",
     clientId: "u6q4sghbCdyCtkTpr",
     secret: secret("meteor-developer"),
-    loginStyle: "popup"
+    loginStyle: loginStyle
   });
 
 });
